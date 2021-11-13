d36d90c7f57e8d107a4e0585883c5c8caa34ca9c
정서연 <jjungseoyeon@naver.com>
2021-11-13 19:34:29 +0900

    start

---
+++ src/main/java/com/daelim/paguda/PagudaApplication.java
@@ -0,0 +1,13 @@
+package com.daelim.paguda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagudaApplication.class, args);
+	}
+
+}
