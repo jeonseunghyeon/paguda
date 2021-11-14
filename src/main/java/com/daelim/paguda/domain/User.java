5f200285ad1a625c8692c1ec18cb4a722bc45706
정서연 <jjungseoyeon@naver.com>
2021-11-14 18:25:46 +0900

    Add User

--- src/main/java/com/daelim/paguda/domain/User.java
+++ src/main/java/com/daelim/paguda/domain/User.java
@@ -9,7 +9,7 @@ import javax.persistence.*;
 @Getter   // lombok으로 getter 자동으로 만들어 줌
 @NoArgsConstructor  // 기본생성자 만들어 줌
 @Entity  // 테이블과 연계됨을 스프링에게 알려줌
-public class User extends Timestamped{  // 생성,수정 시간을 자동으로 만들어줌
+public class User extends Timestamped {  // 생성,수정 시간을 자동으로 만들어줌
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
