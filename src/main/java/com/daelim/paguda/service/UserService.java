16bf0b7d3f9625ca4e717c72be552003d1242f64
정서연 <jjungseoyeon@naver.com>
2021-11-18 18:45:10 +0900

    Solving problems about spring security

--- src/main/java/com/daelim/paguda/service/UserService.java
+++ src/main/java/com/daelim/paguda/service/UserService.java
@@ -3,10 +3,14 @@ package com.daelim.paguda.service;
 import com.daelim.paguda.domain.UserRepository;
 import com.daelim.paguda.dto.UserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +24,4 @@ public class UserService {
         userDto.encodePassword(encoder.encode(userDto.getPassword()));
         return userRepository.save(userDto.toEntity()).getId();
     }
-
 }
